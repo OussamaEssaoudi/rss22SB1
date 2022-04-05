92aef8097d4691896f16084ee566fd687c64e571
OussamaEssaoudi <essaoudi37@gmail.com>
2022-04-05 23:51:05 +0200

    premier commit

---
+++ src/main/java/fr/univrouen/rss22/controllers/IndexController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package fr.univrouen.rss22.controllers;
+
+/**
+ * @author NANOTECH
+ *
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+ 
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "Hello rss22 !";
+    }
+}
